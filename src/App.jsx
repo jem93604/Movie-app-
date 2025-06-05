08c2324e6ccc694bdbffa69db9ad2c88abb7ae76
Jos Ephraim <josephraimofficial@gmail.com>
2025-06-05 14:10:32 +0530

    set up the api mechanism but not he search funtion can now list the movies in the terminal

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,47 @@
 import Search from "./components/Search";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+
+const API_BASE_URL = "https://api.themoviedb.org/3";
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+const API_OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization: `Bearer ${API_KEY}`,
+  },
+};
+
 const App = () => {
-  const [searchTerm, setSearchTerm] = useState("I am bat man ");
+  const [searchTerm, setSearchTerm] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [movieList, setMovieList] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const fetchmovies = async () => {
+    setIsLoading(true);
+    setErrorMessage("");
+    try {
+      const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+      const response = await fetch(endpoint, API_OPTIONS);
+      if (!response.ok) {
+        throw new Error("Failed to fetch movies ");
+      }
+      const data = response.json();
+
+      if (data.response === false) {
+        setErrorMessage(data.message || "Failed to fetch movies");
+        setMovieList([]);
+        return;
+      }
+      setMovieList(data.results || []);
+    } catch (error) {
+      setErrorMessage(`${error} while fetching movies try again later `);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+  useEffect(() => {
+    fetchmovies();
+  }, [searchTerm]);
   return (
     <main>
       <div className="pattern" />
@@ -11,8 +51,13 @@ const App = () => {
           <h1>
             Find <span className="text-gradient">Movies</span> without hassle
           </h1>
+          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
         </header>
-        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+        <section className="all-movies">
+          <h1>All movies</h1>
+
+          {errorMessage ? <p className="text-red-600">{errorMessage}</p> : null}
+        </section>
       </div>
     </main>
   );
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,8 +1,18 @@
 const search = ({ searchTerm, setSearchTerm }) => {
-  setSearchTerm("wow");
   return (
-    <div className="text-white">
-      Search <p>{searchTerm}</p>
+    <div className="search">
+      <div>
+        <img
+          src="https://img.icons8.com/sf-black/128/FFFFFF/search.png"
+          alt="search logo but replaced by react log for now "
+        />
+        <input
+          type="text"
+          placeholder="Search for movies..."
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+      </div>
     </div>
   );
 };
